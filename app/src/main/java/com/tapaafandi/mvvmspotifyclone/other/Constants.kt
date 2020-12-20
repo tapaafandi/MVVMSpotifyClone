e27b8f69dd76c8c76c724737703ad2d5957b3aee
Mustafa Afandi <tapaafandi@gmail.com>
2020-12-20 15:22:08 +0700

    music notification

--- app/src/main/java/com/tapaafandi/mvvmspotifyclone/other/Constants.kt
+++ app/src/main/java/com/tapaafandi/mvvmspotifyclone/other/Constants.kt
@@ -3,4 +3,6 @@ package com.tapaafandi.mvvmspotifyclone.other
 object Constants {
 
     const val SONG_COLLECTION = "songs"
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
 }
