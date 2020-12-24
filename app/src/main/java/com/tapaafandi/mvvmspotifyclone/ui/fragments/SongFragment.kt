9bdd1a2c2e631c8d191844b3aa8d5a68b6a7d914
Mustafa Afandi <tapaafandi@gmail.com>
2020-12-24 19:15:31 +0700

    seekbar

--- app/src/main/java/com/tapaafandi/mvvmspotifyclone/ui/fragments/SongFragment.kt
+++ app/src/main/java/com/tapaafandi/mvvmspotifyclone/ui/fragments/SongFragment.kt
@@ -1,19 +1,24 @@
 package com.tapaafandi.mvvmspotifyclone.ui.fragments
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import android.view.View
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.RequestManager
 import com.tapaafandi.mvvmspotifyclone.R
 import com.tapaafandi.mvvmspotifyclone.data.entities.Song
+import com.tapaafandi.mvvmspotifyclone.exoplayer.isPlaying
 import com.tapaafandi.mvvmspotifyclone.exoplayer.toSong
 import com.tapaafandi.mvvmspotifyclone.other.Status
 import com.tapaafandi.mvvmspotifyclone.ui.viewmodels.MainViewModel
 import com.tapaafandi.mvvmspotifyclone.ui.viewmodels.SongViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_song.*
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -27,10 +32,47 @@ class SongFragment : Fragment(R.layout.fragment_song) {
 
     private var curPlayingsong: Song? = null
 
+    private var playbackState: PlaybackStateCompat? = null
+
+    private var shouldUpdateSeekbar = true
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
         subscribeToObservers()
+
+        ivPlayPauseDetail.setOnClickListener {
+            curPlayingsong?.let {
+                mainViewModel.playOrToggleSong(it, true)
+            }
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    setCurPlayerTimeToTextView(progress.toLong())
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                shouldUpdateSeekbar = false
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                seekBar?.let {
+                    mainViewModel.seekTo(it.progress.toLong())
+                    shouldUpdateSeekbar = true
+                }
+            }
+        })
+
+        ivSkipPrevious.setOnClickListener {
+            mainViewModel.skipToPreviousSong()
+        }
+
+        ivSkip.setOnClickListener {
+            mainViewModel.skipToNextSong()
+        }
     }
 
     private fun updateTitleAndSongImage(song: Song) {
@@ -59,7 +101,29 @@ class SongFragment : Fragment(R.layout.fragment_song) {
             curPlayingsong = it.toSong()
             updateTitleAndSongImage(curPlayingsong!!)
         }
+        mainViewModel.playbackState.observe(viewLifecycleOwner) {
+            playbackState = it
+            ivPlayPauseDetail.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+            seekBar.progress = it?.position?.toInt() ?: 0
+        }
+        songViewModel.curPlayerPosition.observe(viewLifecycleOwner) {
+            if (shouldUpdateSeekbar) {
+                seekBar.progress = it.toInt()
+                setCurPlayerTimeToTextView(it)
+            }
+        }
+        songViewModel.curSongDuration.observe(viewLifecycleOwner) {
+            seekBar.max = it.toInt()
+            val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+            tvSongDuration.text = dateFormat.format(it)
+        }
+    }
 
+    private fun setCurPlayerTimeToTextView(ms: Long) {
+        val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+        tvCurTime.text = dateFormat.format(ms)
     }
 
 }
